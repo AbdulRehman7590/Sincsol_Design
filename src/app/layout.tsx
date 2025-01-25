f4cee1fe1bdaca753dac403b4dc71963050a0bda
AbdulRehman <abrehman1748@gmail.com>
2025-01-25 19:56:40 +0500

    Setting popins font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,22 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import '../styles/globals.css'
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "../styles/globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ subsets: ["latin"], weight: ["400", "700"] });
 
 export const metadata: Metadata = {
-  title: 'SIncSol - Software Development Company',
-  description: 'Empowering Your Business with Tailored Software Solutions',
-}
+  title: "SIncSol - Software Development Company",
+  description: "Empowering Your Business with Tailored Software Solutions",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
-  )
+  );
 }
-
